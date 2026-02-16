b5583482f752f7c9d15fec773fb897ed1030ae35
Jenny <jennywei0102@gmail.com>
2026-02-16 03:56:06 +0100

    Add db.py and main.py

--- backend/db.py
+++ backend/db.py
@@ -2,6 +2,5 @@ from pymongo import MongoClient
 import os
 
 MONGO_URI = os.getenv("MONGO_URI")
-
 client = MongoClient(MONGO_URI)
-db = client["asset_manager"]
+db = client["assetManager"]
--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,7 @@
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi import FastAPI
 from routes.tea import router as tea_router
-from dotenv import load_dotenv
 
-load_dotenv()
 app = FastAPI()
 app.include_router(tea_router)
-
-@app.get("/")
-def root():
-    return {"messgae": "Hello FastAPI"}
-
-@app.post("/echo")
-def echo(data: dict):
-    return {
-        "received": data
-    }
