175db82928cdc86510988745967eb477c9eecd0a
Jenny <jennywei0102@gmail.com>
2026-02-16 03:56:45 +0100

    Add tea model and route (POST & GET)

--- backend/models/tea.py
+++ backend/models/tea.py
@@ -4,14 +4,18 @@ from datetime import datetime, timezone
 def utcnow():
     return datetime.now(timezone.utc)
 
-print(utcnow())
-
 class Tea(BaseModel):
-    name: str | None = None
-    origin: str = Field(..., json_schema_extra={"example": "Alishan"})
+    name: str = Field(..., json_schema_extra={"example": "High mountain oolong tea"})
+    origin: str | None = Field(None, json_schema_extra={"example": "Alishan"})
+    genre: str | None = Field(None, json_schema_extra={"example": "oolong"})
+    roastLevel: int | None = Field(None, json_schema_extra={"example": 50})
     harvestTime: int | None = Field(None, json_schema_extra={"example": 202506})
-    price: int | None = Field(None, json_schema_extra={"example": 300})
+    weight: int | None = Field(None, json_schema_extra={"example": 600})
+    quantity: int | None = Field(None, json_schema_extra={"example": 3})
     created_at: datetime = Field(default_factory=utcnow)
-    
-tea = Tea(origin="Sun Moon Lake", harvestTime=202412, price=1000)
-print(tea.model_dump()) 
+    owner_id: str
+
+# cost: int = Field(..., json_schema_extra={"example": 300})
+# price: int | None = Field(None, json_schema_extra={"example": 300}) 
+# price per unit size in UI
+# roastLevel: 0~100%
--- backend/routes/tea.py
+++ backend/routes/tea.py
@@ -5,12 +5,25 @@ from db import db
 router = APIRouter(prefix="/tea", tags=["Tea"])
 
 @router.post("/")
-async def create_tea(tea: Tea):
+def create_tea(tea: Tea):
     tea_dict = tea.model_dump()
     result = db.teas.insert_one(tea_dict)
+    tea_dict['_id'] = str(result.inserted_id)
     
     return {
         "message": "Tea created",
         "tea": tea_dict,
         "id": str(result.inserted_id)
     }
+
+@router.get("/", response_model=list[Tea])
+def list_teas():
+    return list(db.teas.find())
+
+@router.patch("/")
+def edit_tea():
+    return
+
+@router.delete("/")
+def remove_tea():
+    return
