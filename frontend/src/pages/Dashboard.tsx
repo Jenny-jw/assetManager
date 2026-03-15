b2597375a63f7178f3cf7ae8a5978cd2dfe16e6b
Jenny <jennywei0102@gmail.com>
2026-03-15 15:59:09 +0800

    Add Summary in dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import Summary from "../components/Summary";
+import axios from "axios";
+import type { Asset } from "../types/Asset";
+
 const Dashboard = () => {
+  const [assets, setAssets] = useState<Asset[]>([]);
+
+  useEffect(() => {
+    axios.get("/api/tea").then((res) => {
+      setAssets(res.data);
+    });
+  }, []);
+
   return (
-    <>
-      <h1>Dashboard</h1>
-      <p>Welcome~</p>
-    </>
+    <div className="p-6 space-y-6">
+      <h1 className="text-3xl font-bold">Asset Manager Dashboard</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <Summary assets={assets} />
+      </div>
+    </div>
   );
 };
 
