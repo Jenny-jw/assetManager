36a7605daeaa9568a716428c85ceaded224e917a
Jenny <jennywei0102@gmail.com>
2026-03-15 15:54:48 +0800

    Add CORS and proxy

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,18 @@ from dotenv import load_dotenv
 load_dotenv()
 from fastapi import FastAPI
 from routes.tea import router as tea_router
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+]
 
 app = FastAPI()
-app.include_router(tea_router)
+app.include_router(tea_router, prefix="/api")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
