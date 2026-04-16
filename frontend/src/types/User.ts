520643108d6b8f52d70027ec6caa6bb807a9ab6c
Jenny <jennywei0102@gmail.com>
2026-04-16 15:19:44 +0800

    Add User type

---
+++ frontend/src/types/User.ts
@@ -0,0 +1,15 @@
+export const UserRole = {
+  ADMIN = "admin",
+  USER = "user",
+} as const;
+
+export type UserRole = (typeof UserRole)[keyof typeof UserRole];
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  role: UserRole;
+  created_at: string;
+  is_active: boolean;
+};
