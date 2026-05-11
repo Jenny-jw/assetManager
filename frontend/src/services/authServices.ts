46ef3241983ce0d4c0846f2b9588b4f6183873ec
Jenny <jennywei0102@gmail.com>
2026-05-11 12:09:07 +0800

    Add authService in frontend

---
+++ frontend/src/services/authServices.ts
@@ -0,0 +1,20 @@
+import api from "../lib/axios";
+
+export const signup = async (name: string, email: string, password: string) => {
+  const response = await api.post("/auth/signup", { name, email, password });
+  return response.data;
+};
+
+export const login = async (email: string, password: string) => {
+  const response = await api.post("/auth/login", { email, password });
+  return response.data;
+};
+
+export const logout = async () => {
+  await api.post("/auth/logout");
+};
+
+export const getCurrentUser = async () => {
+  const response = await api.get("/auth/me");
+  return response.data;
+};
