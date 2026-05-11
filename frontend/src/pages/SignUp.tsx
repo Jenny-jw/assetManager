1068155c4944c52dfed81153292ed808b5db0f6c
Jenny <jennywei0102@gmail.com>
2026-05-11 12:08:01 +0800

    Signup page error handling

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,9 +1,45 @@
+import type React from "react";
+import { signup } from "../services/authServices";
+import { useState, type SubmitEventHandler } from "react";
+
+declare module "react-router" {
+  interface FetcherFormProps {
+    onSubmit?: SubmitEventHandler<HTMLFormElement>;
+  }
+}
+
 const SignUp = () => {
+  const [errMsg, setErrMsg] = useState<string>("");
+  const handleSubmit = async (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    try {
+      await signup(name, email, password);
+      console.log("User signed up! 🎉");
+    } catch (err) {
+      if (err instanceof Error) {
+        const msg = err.message;
+        if (msg === "Email already registered") {
+          setErrMsg("This email is already registered. Please log in instead.");
+        } else {
+          setErrMsg("An error occurred during sign up. Please try again.");
+        }
+      } else {
+        console.error("Unexpected error during sign up:", err);
+      }
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="bg-lime-600/50 p-8 rounded shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit}>
           <div>
             <label
               htmlFor="name"
@@ -49,6 +85,9 @@ const SignUp = () => {
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
             />
           </div>
+          {errMsg && (
+            <p className="text-red-300 text-sm text-center">{errMsg}</p>
+          )}
           <button
             type="submit"
             className="w-full bg-lime-600 text-white py-2 rounded hover:bg-lime-700 hover:border-lime-500 transition"
