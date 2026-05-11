6d3cdf1a95ee304a435d78ac87c918871c469e5f
Jenny <jennywei0102@gmail.com>
2026-05-11 11:02:54 +0800

    Include user route. Move load_dotenv below imports.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
-load_dotenv()
 from fastapi import FastAPI
 from routes.tea import router as tea_router
+from routes.auth import router as user_router
 from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
 
 origins = [
     "http://localhost:5173",
@@ -10,6 +12,7 @@ origins = [
 
 app = FastAPI()
 app.include_router(tea_router, prefix="/api")
+app.include_router(user_router, prefix="/api")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
