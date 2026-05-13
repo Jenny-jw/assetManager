7cf91bc0452aa97becfeeeb4cd75a71ae90fde03
Jenny <jennywei0102@gmail.com>
2026-05-13 17:34:09 +0800

    Add handleLogin. Add error message and navigation. Add name attributes in <input>.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { login } from "../services/authServices";
 
 const Login = () => {
+  const [errMsg, setErrMsg] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setErrMsg("");
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    try {
+      const data = await login(email, password);
+
+      console.log("Data: ", data);
+
+      navigate("/dashboard");
+    } catch (err) {
+      if (err instanceof Error && err.message) {
+        setErrMsg(err.message);
+      } else {
+        console.error("Unexpected error during login:", err);
+        setErrMsg("An unexpected error occurred during login.");
+      }
+    }
+  };
   return (
     <div className="min-h-screen grid grid-cols-1 md:grid-cols-2">
       {/* Intro on the left */}
@@ -19,21 +47,28 @@ const Login = () => {
 
       {/* Login on the right */}
       <div className="flex items-center justify-center p-6">
-        <div className="w-full max-w-md space-y-6">
+        <form onSubmit={handleLogin} className="w-full max-w-md space-y-6">
           <h2 className="text-2xl font-bold text-center">Log In</h2>
 
           <input
             type="email"
+            name="email"
             placeholder="Email"
             className="w-full border p-3 rounded"
           />
           <input
             type="password"
+            name="password"
             placeholder="Password"
             className="w-full border p-3 rounded"
           />
-
-          <button className="w-full bg-lime-600 text-white py-3 rounded">
+          {errMsg && (
+            <p className="text-red-300 text-sm text-center">{errMsg}</p>
+          )}
+          <button
+            type="submit"
+            className="w-full bg-lime-600 text-white py-3 rounded"
+          >
             Log In
           </button>
 
@@ -43,7 +78,7 @@ const Login = () => {
               Sign Up
             </Link>
           </p>
-        </div>
+        </form>
       </div>
     </div>
   );
