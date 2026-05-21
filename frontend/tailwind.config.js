40eb632d707fdc15dc55577b1a303e6eee8785b0
Jenny <jennywei0102@gmail.com>
2026-05-21 18:44:35 +0800

    Aff autoprefixer, postcss packages. Remove npx, init packages

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue,svelte}"],
+  theme: {
+    extend: {
+      colors: {
+        warmBg: "#ece2ba",
+        oliveText: "#64794d",
+      },
+    },
+  },
+  plugins: [],
+};
