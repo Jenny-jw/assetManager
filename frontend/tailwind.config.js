e0bebb52c19d352003254b31b5a6a7927ab0e7f4
Jenny <jennywei0102@gmail.com>
2026-05-22 14:01:45 +0800

    Edit postcss config and tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,4 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue,svelte}"],
-  theme: {
-    extend: {
-      colors: {
-        warmBg: "#ece2ba",
-        oliveText: "#64794d",
-      },
-    },
-  },
   plugins: [],
 };
